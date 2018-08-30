94967aa23bc704496b473e0bdd9be653de56e1e6
rldan <rldan@thoughtworks.com>
2018-08-30 15:06:59 +0800

    step-6 add simple pre filter

---
+++ src/main/java/com/otr/wuhan/learning/apigateway/filters/PreFilter.java
@@ -0,0 +1,31 @@
+package com.otr.wuhan.learning.apigateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PreFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("XXXXXXXXXXXXX");
+        return null;
+    }
+}
