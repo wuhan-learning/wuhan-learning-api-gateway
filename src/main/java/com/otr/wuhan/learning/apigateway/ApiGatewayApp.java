9ef24a8a726a2e5bea8afc7731025994ba3a6b81
swyin <swyin@thoughtworks.com>
2018-09-05 23:39:59 +0800

    [shangwei] add hystrix.

--- src/main/java/com/otr/wuhan/learning/apigateway/ApiGatewayApp.java
+++ src/main/java/com/otr/wuhan/learning/apigateway/ApiGatewayApp.java
@@ -2,10 +2,12 @@ package com.otr.wuhan.learning.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringCloudApplication
 @EnableZuulProxy
+@EnableHystrixDashboard
 public class ApiGatewayApp {
 
     public static void main(String[] args) {
---
+++ src/test/resources/locustfile.py
@@ -0,0 +1,22 @@
+from locust import HttpLocust, TaskSet, task
+
+class GetGoodsById(TaskSet):
+    @task(1)
+    def getGoodsById(self):
+        self.client.get("/user/goods/101")
+
+class GetAllGoods(TaskSet):
+    @task(1)
+    def getAllGoods(self):
+        self.client.get("/user/goods")
+
+class UserBehavior(TaskSet):
+    tasks = {
+        GetGoodsById: 1,
+        GetAllGoods: 1,
+    }
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 2000
+    max_wait = 5000
